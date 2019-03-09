dfaac6bf0b1477b3e7d6cca1cbbb2f1d9ac44152
Foo-Manroot <foomanroot@disroot.org>
2019-03-10 00:01:27 +0100

    First approach to do it using the PHP library instead of Google's JS

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -1,37 +1,7 @@
 <?php
-/**
- * Needed to work with the library jumbojett/openid-connect-php
- */
-require __DIR__ . '/../vendor/autoload.php';
-
-/**
- * File with all the configuration and tokens
- */
-$config = require (__DIR__ . '/../config.php');
 
 
-/**
- * Checks if the selected provider has been configured
- *
- * @param array $config
- *		An array with the configuration of the webpage
- *
- * @param string $provider
- *		A string with the name of the provider, as specified in the configuration
- *
- * @return boolean
- */
-function check_provider ($config, $provider)
-{
-	return (
-		array_key_exists ($provider, $config)
-		&& array_key_exists ("URL", $config [$provider])
-		&& array_key_exists ("ID", $config [$provider])
-		&& array_key_exists ("Secret", $config [$provider])
-	);
-}
-
-
+require __DIR__ . '/../include/utils.php';
 
 ?>
 <!DOCTYPE html>
@@ -41,50 +11,30 @@ function check_provider ($config, $provider)
 	<meta charset="UTF-8">
 	<title>OpenID Connect assignment</title>
 
+
+	<!-- From https://github.com/necolas/css3-social-signin-buttons -->
+	<link rel="stylesheet" href="css/auth-buttons.css">
 	<link rel="stylesheet" href="css/style.css">
-
-<?php
-/* Initialization of Google */
-if (check_provider ($config, "Google"))
-{
-?>
-	<!-- Extracted from https://developers.google.com/identity/sign-in/web/build-button -->
-	<meta name="google-signin-client_id" content="<?php  echo $config ["Google"]["ID"]; ?>">
-<?php
-}
-?>
-
 
 </head>
 <body>
 
-	<div>
+	<div id="login-panel">
 		<h1>Login using one of the following OpenID Connect providers</h1>
-		<!-- Extracted from https://developers.google.com/identity/sign-in/web/build-button -->
-		<div id="google-signin">Google</div>
-		<script>
-			function onSuccess (googleUser) {
-
-				alert ('Logged in as: ' + googleUser.getBasicProfile ().getName ());
-			}
-			function onFailure (error) {
-
-				alert (error);
-			}
-			function renderButton () {
-				gapi.signin2.render ('google-signin', {
-					'scope': 'profile email',
-					'width': 240,
-					'height': 50,
-					'longtitle': true,
-					'theme': 'dark',
-					'onsuccess': onSuccess,
-					'onfailure': onFailure
-				});
-			}
-		</script>
-		<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
-
+			<?php
+/* Google */
+if (check_provider ("google"))
+{
+?>
+		<form action="/login.php" method="POST" accept-charset="utf-8">
+			<input type="hidden" value="google" name="provider" />
+			<button type="submit" class="btn-auth btn-google" id="google">
+				Sign in with Google
+			</button>
+		</form>
+<?php
+}
+?>
 
 	</div>
 
